8479f8e8d466037b125cbca6b8a70c720690f2e2
AlecMalenfant001 <malenfantalec@gmail.com>
2024-02-27 12:08:20 -0600

    Add aws amplify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
+  experimental: {
+    appDir: true,
   },
-  output: "export",
 };
+
 module.exports = nextConfig;
