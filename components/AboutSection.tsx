9916f8c76b79ed3341f0b1744d366c23a725d7a3
Jake Holmes <jakerichmondholmes@gmail.com>
2024-05-18 14:08:12 -0500

    Update AboutSection.tsx

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -30,22 +30,22 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
+              Hi, my name is Jake and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              <span className="font-bold">{" driven"}</span> Customer Success Manager
+              based in Austin, Texas.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
+              I graduated from Southwestern University in 2019
+              with a BA in Business and have been working in the
               field ever since.
             </p>
             <br />
             <p>
               I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making YouTube videos,
+              From reading, playing sports, traveling, to playing guitar,
               I am always seeking new experiences and love to keep myself
               engaged and learning new things.
             </p>
