6838300ce7129d4dab66bac216fc7b35a3dc7baa
AlecMalenfant001 <malenfantalec@gmail.com>
2024-09-24 14:00:50 -0500

    update about me

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 const skills = [
+  { skill: "CompTIA A+ Certified" },
   { skill: "Java" },
   { skill: "C++" },
   { skill: "Python" },
@@ -29,6 +30,7 @@ const AboutSection = () => {
         </h1>
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          {/* About Me Text */}
           <div className="md:w-1/2 ">
             <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
               Get to know me!
@@ -43,22 +45,29 @@ const AboutSection = () => {
             <br />
             <p>
               Attending both Ivy Tech and Purdue University Northwest, I have
-              accumulated a wide variety of skill that help me tackle a wide
-              range of projects from devloping Java apps to help keep track of
-              my Rubik&#39;s Cube solves, to a React based web tool to monitor
-              and log the status of chromebooks at an institution. Even after
-              having this wide set of tools in my toolbag, I wake up every day
-              wanting to either tackle a new technology or dig even deeper into
-              one I already know.
+              accumulated various skills that help me tackle a wide range of
+              projects. From writing Python scripts to managing a local server
+              to developing and deploying multiple websites, I have the skills
+              to turn ideas into reality. Some projects that I have worked on
+              include :
             </p>
+            <ul>
+              <li>Ai image generation web app</li>
+              <li>Chromebook repository web app</li>
+              <li>Campus navigation web app</li>
+              <li>Home server running multiple Docker containers</li>
+              <li>
+                Rubik&#39s Cube scramble generator and solve timer Java
+                application
+              </li>
+            </ul>
             <br />
             <p>
-              Outside of tech, there are many things that interest me in my day
-              to day life. I love singing, dancing, solving twisty puzzles like
-              Rubik&#39;s cubes, sim racing, skating/longboarding, and so much
-              more. I always say that I am willing to try anything twice. Even
-              if there is a mountain of odds stacked against me, I will never
-              back down from giving it my all.
+              Outside of tech, I have many hobbies. I always say that I am
+              willing to try anything twice. I will never hesitate to give it my
+              all and get back up when I fall. I love singing, dancing, solving
+              puzzles like Rubik&#39;s cubes, sim racing, skateboarding,
+              longboarding, and more.
             </p>
             <br />
             <p>
@@ -68,8 +77,15 @@ const AboutSection = () => {
               </span>{" "}
               , super outgoing, and always up for a new challenge
             </p>
+            <section id="contact">
+              <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+                Contact me!
+              </h1>
+              <p>Email : malenfantalec@gmail.com</p>
+            </section>
           </div>
           <div className="text-center md:w-1/2 md:text-left">
+            {/* Skills */}
             <h1 className="text-2xl font-bold mb-6">My Skills</h1>
             <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
               {skills.map((item, idx) => {
